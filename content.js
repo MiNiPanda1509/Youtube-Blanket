7d6b6ff6bab922509038355ff8ce5721d313e262
panda11:11 <minipanda1509@pomo.com>
2024-01-27 17:00:25 +0530

    Modifying user experience of the blanket
    Signed-off-by: panda11:11 <minipanda1509@pomo.com>

--- content.js
+++ content.js
@@ -1,11 +1,29 @@
+let ccs_props = {'opacity' : 0,
+                'pointer-events': 'none' }
+
+const elements = ['#content.style-scope.ytd-rich-item-renderer',
+                  '#dismissible.style-scope.ytd-compact-video-renderer',
+                  '#dismissible.style-scope.ytd-compact-radio-renderer' ]
+
 chrome.runtime.onMessage.addListener( (req, sender, res) => {
+                
     if(req.todo == "toggle"){
 
         let state;
         state = 1 - req.newState
-        $('#content.style-scope.ytd-rich-item-renderer').css('opacity', state)
-        $('#dismissible.style-scope.ytd-compact-video-renderer').css('opacity', state)
-        $('#dismissible.style-scope.ytd-compact-radio-renderer').css('opacity', state)
+
+        ccs_props.opacity = state
+
+        if(state == 0 ) ccs_props["pointer-events"] = 'none'
+        else ccs_props["pointer-events"] = 'auto'
+
+        const keys = Object.keys(ccs_props);
+
+        elements.forEach(ele => {
+            keys.forEach(key => {
+                $(ele).css(key, ccs_props[key])
+            })              
+        })
     }
 
 })
